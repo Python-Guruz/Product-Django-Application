b4c773dda5c32c33631b142f509b5c17cb4b5d5e
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2021-09-17 18:15:35 +0300

    created get and post requests in category/views.py

--- category/views.py
+++ category/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from category.serializers import CategorySerializer
+from category.models import products
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(["GET", "POST"])
+
+def category(request):
+    if request.method == 'GET':
+        Category = category.objects.all()
+        serializer = CategorySerializer(Category, many=True)
+        return Response({'data':serializer.data},status.HTTP_200_OK)
+
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid:
+
+            serializer.save()
+            return Response({"message":"The product has been created successfully",
+                            "data" : serializer.data
+                            
+            },status.HTTP_201_CREATED)
+        
+
