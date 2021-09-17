9031d945a7c4f7005b4ef6c84ab97cbdb2dab327
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2021-09-17 16:57:20 +0300

    registered category in settings.py

---
+++ category/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+import uuid
+# Create your models here.
+
+class Category(models.Model):
+        id = models.UUIDField(primary_key = True,default=uuid.uuid4,editable=False)
+        type = models.CharField(max_length=300, blank=False, null= False)
+        manufacture_date =models.DateField(manufacture_date= 12-7-21, null=False,blank=False)
+        expiry_date = models.DateField(expiry_date= 30-7-25, null= False, blank = False)
