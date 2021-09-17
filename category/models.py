0a322ba87ce867f60e000caef2fc20617e213725
Halimah Bukirwa <halimahbukirwa24@gmail.com>
2021-09-17 17:40:36 +0300

    made database migrations of category data

--- category/models.py
+++ category/models.py
@@ -5,5 +5,9 @@ import uuid
 class Category(models.Model):
         id = models.UUIDField(primary_key = True,default=uuid.uuid4,editable=False)
         type = models.CharField(max_length=300, blank=False, null= False)
-        manufacture_date =models.DateField(manufacture_date= 12-7-21, null=False,blank=False)
-        expiry_date = models.DateField(expiry_date= 30-7-25, null= False, blank = False)
+        #manufacture_date =models.DateField(manufacture_date= 12-7-21, null=False,blank=False)
+        #expiry_date = models.DateField(expiry_date= 30-7-25, null= False, blank = False)
+
+        def __str__(self):
+                return self.name
+        
